fc088d8a2077972b0f5ffb9ebc4431674a39d53b
Poojagadhewal2006 <poojagadhewal2006@gmail.com>
2025-09-14 10:29:32 +0530

    Add files via upload

---
+++ Array1.java
@@ -0,0 +1,12 @@
+public class Array1 {
+    public static void main(String[] args) {
+     //int num = 12345;
+     // output = 52341;
+   
+     
+   
+     char ch = 5;
+     int a = ch;
+     System.out.println(a);
+    }
+}
